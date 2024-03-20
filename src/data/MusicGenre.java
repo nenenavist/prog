fe3be54dafd4d76a5fbe063e6a3290b0f4d9f6e7
nenenavist <151150017+nenenavist@users.noreply.github.com>
2024-03-20 12:06:05 +0300

    Delete src directory

--- src/data/MusicGenre.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-public enum MusicGenre {
-    PROGRESSIVE_ROCK,
-    HIP_HOP,
-    PSYCHEDELIC_CLOUD_RAP,
-    PUNK_ROCK,
-    BRIT_POP;
-}
--- src/exceptions/NoElementException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class NoElementException extends Exception {
-    public NoElementException(Integer id) {
-        super("No element in collection with id: " + id);
-    }
-}
