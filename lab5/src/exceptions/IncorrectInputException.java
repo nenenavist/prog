7f89daf8ef5c8cbdf912f4ca5e49180697117561
nenenavist <151150017+nenenavist@users.noreply.github.com>
2024-04-03 16:50:30 +0300

    Delete lab5/src directory

--- lab5/src/exceptions/IncorrectInputException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class IncorrectInputException extends Exception {
-    public IncorrectInputException(String data){
-        super("Wrong input argument: " + data);
-    }
-}
--- lab5/src/exceptions/UsedIdException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UsedIdException extends Exception {
-    public UsedIdException(int id){
-        super("Id: " + id + " already used");
-    }
-}
