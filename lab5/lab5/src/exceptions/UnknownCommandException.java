a67ed2ee150de1b48d5f83a092f30c839a36d8f9
nenenavist <151150017+nenenavist@users.noreply.github.com>
2024-04-06 01:30:46 +0300

    Delete lab5/lab5 directory

--- lab5/lab5/src/exceptions/UnknownCommandException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UnknownCommandException extends Exception{
-    public UnknownCommandException(String commandName) {
-        super("Unknow command: " + commandName);
-    }
-}
--- lab5/lab5/src/exceptions/UsedIdException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UsedIdException extends Exception {
-    public UsedIdException(int id){
-        super("Id: " + id + " already used\nесли вы выбили эту ошибку, то добавьте + пару баллов пж((((");
-    }
-}
